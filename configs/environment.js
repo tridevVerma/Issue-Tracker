9aed34939250320e3c50ab68231f97e2bb1ca7bb
Tridev Verma <tv072000@gmail.com>
2023-05-14 17:16:52 +0530

    Add Gulp and its configuations

--- configs/environment.js
+++ configs/environment.js
@@ -11,12 +11,14 @@ const development = {
 const production = {
   name: "development",
   domain: "localhost",
-  mongodb_URI:
-    "mongodb+srv://tridev23:s1n3qN0vf5NLeTkG@cluster0.bljbc4x.mongodb.net/issue_tracker_db?retryWrites=true&w=majority",
+  mongodb_URI: `mongodb+srv://${process.env.ISSUE_TRACKER_DB_USERNAME}:${process.env.ISSUE_TRACKER_DB_PASSWORD}@cluster0.bljbc4x.mongodb.net/issue_tracker_db?retryWrites=true&w=majority`,
   db_name: "issue_tracker_db",
   server_port: 8000,
   asset_path: "./assets",
   views_path: "./views",
 };
 
-module.exports = production;
+module.exports =
+  eval(process.env.NODE_ENV) == undefined
+    ? development
+    : eval(process.env.NODE_ENV);
